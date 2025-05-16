6b136d4d843827cfb868e91598356120d8dea85b
diaHTML <kahabanz@gmail.com>
2025-05-16 17:41:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,60 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-canvas.width = 400;
-canvas.height = 600;
-
-let basket = { x: 180, y: 550, width: 60, height: 30 };
-let pistachios = [];
-let score = 0;
-let level = 1;
-let messages = [
-  "Ты собираешь фисташки рачком ах!",
-  "Дяна, ты самая вкусная лэ саленая там ням ням!",
-  "Скоро фисташковое королевство будет твоим!",
-  "Магазин ждёт тебя, сучка!"
-];
-
-let currentMessage = "";
-
-function drawBasket() {
-  ctx.fillStyle = '#84c18e';
-  ctx.fillRect(basket.x, basket.y, basket.width, basket.height);
+body {
+  margin: 0;
+  font-family: 'Comic Sans MS', cursive;
+  background-image: url('images/clouds-bg.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  text-align: center;
+  color: #ff69b4;
 }
 
-function drawPistachios() {
-  ctx.fillStyle = '#d6caa0';
-  pistachios.forEach(p => {
-    ctx.beginPath();
-    ctx.ellipse(p.x, p.y, 10, 15, 0, 0, Math.PI * 2);
-    ctx.fill();
-  });
+h1 {
+  font-size: 2.5em;
+  margin-top: 20px;
 }
 
-function drawScore() {
-  document.getElementById('score').innerText = score;
-  document.getElementById('message').innerText = currentMessage;
+.subtitle {
+  margin-bottom: 20px;
+  font-style: italic;
 }
 
-function updatePistachios() {
-  pistachios.forEach(p => p.y += 2 + level * 0.3);
-  pistachios = pistachios.filter(p => {
-    if (
-      p.y > basket.y &&
-      p.x > basket.x &&
-      p.x < basket.x + basket.width
-    ) {
-      score++;
-      if (score % 10 === 0) {
-        level++;
-        currentMessage = messages[Math.floor(Math.random() * messages.length)];
-      }
-      return false;
-    }
-    return p.y < canvas.height;
-  });
+#game {
+  position: relative;
+  width: 100%;
+  height: 500px;
+  overflow: hidden;
 }
 
-function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBasket();
-  drawPistachios();
-  drawScore();
-  updatePistachios();
-  requestAnimationFrame(gameLoop);
+#basket {
+  width: 80px;
+  height: 60px;
+  background-image: url('images/hat.png');
+  background-size: cover;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
 }
 
-setInterval(() => {
-  pistachios.push({
-    x: Math.random() * canvas.width,
-    y: -20
-  });
-}, 1000);
+.fistashka {
+  width: 40px;
+  height: 40px;
+  background-image: url('images/pistachio.png');
+  background-size: cover;
+  position: absolute;
+  top: 0;
+}
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowLeft') basket.x -= 20;
-  if (e.key === 'ArrowRight') basket.x += 20;
-});
+#shop {
+  margin-top: 20px;
+}
 
-gameLoop();
+button {
+  margin: 5px;
+  padding: 10px;
+  background-color: #ffe4e1;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 1em;
+}
